10bc7db247fea27030f0f309ed750dfa4153a696
Marc Destefanis <marc.destefanis.etu@gmail.com>
2016-01-11 15:43:01 +0100

    add play pause

--- app/scripts/controllers/waveSurferTest.js
+++ app/scripts/controllers/waveSurferTest.js
@@ -4,15 +4,30 @@
 angular.module('frontEndApp')
   .controller('WaveSurferTestCtrl', function ($rootScope) {
 
+    var vm = this;
+
     var wavesurfer = WaveSurfer.create({
       container: '#wave',
-      waveColor: 'violet',
-      progressColor: 'purple'
+      waveColor: 'grey',
+      progressColor: 'black'
     });
 
+    /*wavesurfer.init({
+      container: document.querySelector('#wave'),
+      backend: 'MediaElement'
+    });
+
+    document.querySelector('#slider').oninput = function () {
+      wavesurfer.zoom(Number(this.value));
+    };*/
+
     wavesurfer.on('ready', function () {
-      wavesurfer.play();
+      console.log("song ready");
     });
 
     wavesurfer.load('cloud_beat.wav');
+
+    vm.playPause = function(){
+      wavesurfer.playPause();
+    }
   });
