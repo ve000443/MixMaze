3343973599e15712e0d879f3ba4155977937af03
kevinVarandas <kevin.varandas@gmail.com>
2016-02-08 08:29:50 +0100

    ajout du server et des tests serveur pour pouvoir lancer en local si besoin

---
+++ server/server.js
@@ -0,0 +1,355 @@
+var express = require("express");
+var cors = require("cors");
+var mongoose = require('mongoose');
+var fs = require("fs");
+var bodyParser = require('body-parser');
+
+mongoose.connect("mongodb://xythe.xyz:27017/mixmaze");
+
+
+var userSchema = {
+    pseudo: String,
+    password: String,
+    email: String,
+    role: String
+};
+
+var mixSchema = {
+    owner: String,
+    name: String,
+    music: String,
+    data : String
+};
+
+var musicSchema = {
+    musicName: String,
+    musicPath: String,
+    musicFiles : [String]
+};
+
+var starSchema = {
+    mixName: String,
+    userName: String,
+    star: Number
+}
+
+var User = mongoose.model('User', userSchema, 'user');
+var Music = mongoose.model('Music', musicSchema, 'music');
+var Mix = mongoose.model('Mix', mixSchema, 'mix');
+var Star = mongoose.model('Star', starSchema, 'star');
+
+
+/*User.remove({}, function(err){
+        if(err) throw err;
+});
+*/
+
+/*Mix.remove({}, function(err){
+    if (err) throw err;
+});*/
+
+/*Music.remove({}, function(err){
+        if(err) throw err;
+});*/
+
+
+/*
+fs.readdir("./musics", function(err, items) {
+        items.forEach(function(item){
+                fs.readdir("./musics/"+item, function(err, musics){
+                    var music = new Music({ musicName: item, musicPath: "/musics/"+item, musicFiles: musics });
+
+                    music.save(function (err) {
+                        if (err) throw err;
+                    })
+                });
+        });
+});*/
+
+var app = express();
+app.use(cors());
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get("/users", function(req, res){
+    User.find(function (error, results) {
+        if (error) throw error
+        else{
+            res.statusCode = 200;
+            res.send(results);
+        }
+    });
+});
+
+app.get("/users/:pseudo", function(req, res){
+    var query  = User.where({ pseudo: req.params.pseudo });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        else{
+            res.statusCode = 200;
+            res.send(user);
+        }
+    });
+});
+
+app.post("/users", function(req, res){
+    if (!req.body) return res.sendStatus(400)
+    var json = req.body;
+    console.log(json);
+    var query  = User.where({ pseudo: json.pseudo });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        console.log(user);
+        if(user === null){
+            var user = new User({ pseudo: json.pseudo, email: json.email, password: json.password, role: "member" });
+
+            user.save(function (err) {
+                if (err) throw err;
+                else
+                    res.sendStatus(201);
+            });
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.put("/users", function(req, res){
+    if (!req.body) return res.sendStatus(400)
+    var json = req.body;
+    var query  = User.where({ pseudo: json.pseudo });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        if(json.applicant !== undefined && json.applicant === "admin"){
+            user.role = json.role;
+            user.save(function (err) {
+               res.sendStatus(201);
+            });
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.delete("/users/:applicant/:pseudo", function(req, res){
+    var applicant = req.params.applicant;
+    var query  = User.where({ pseudo: req.params.pseudo });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        if(applicant === "admin" && applicant !== undefined){
+            user.remove();
+            res.sendStatus(200);
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.get("/musics", function(req, res){
+    Music.find(function(error, results){
+        if (error) throw error;
+        else{
+            res.statusCode = 200;
+            res.send(results);
+        }
+    })
+});
+
+app.get("/mix", function(req, res){
+    Mix.find(function(error, results){
+        if(error) throw error;
+        else{
+            res.statusCode = 200;
+            res.send(results);
+        }
+    })
+});
+
+app.get("/mix/:musicName", function(req, res){
+    var query  = Mix.where({ music: req.params.musicName });
+    query.find(function (err, mix) {
+        if (err) throw err;
+        if (mix) {
+            res.statusCode = 200;
+            res.send(mix);
+        }
+    });
+});
+
+app.get("/musics/:musicname", function(req, res){
+    var query  = Music.where({ musicName: req.params.musicname });
+    query.findOne(function (err, music) {
+        if (err) throw err;
+        if (music) {
+            res.statusCode = 200;
+            res.send(music);
+        }
+    });
+});
+
+
+
+app.post("/mix", function(req, res){
+    if (!req.body) return res.sendStatus(400)
+    var json = req.body;
+    var query  = User.where({ pseudo: json.owner });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        if(user.role !== undefined && user.role === "member" || user.role === "moderator" || user.role === "admin"){
+            query = Mix.where({ name: json.name });
+            query.findOne(function(err, mix){
+                if(mix === null){
+                    var mix = new Mix({owner: json.owner, name: json.name, music : json.music, data: json.data}) ;
+
+                    mix.save(function (err) {
+                        if (err) throw err;
+                        else
+                            res.sendStatus(201);
+                    });
+                }
+                else{
+                    res.sendStatus(403);
+                }
+            });
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.delete("/mix/:mixname/:owner", function(req, res){
+    var query  = User.where({ pseudo: req.params.owner });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        if(user.role === "member" || user.role === "moderator" || user.role === "admin" && user.role !== undefined){
+            var query  = Mix.where({ name: req.params.mixname, owner: req.params.owner });
+            query.findOne(function(err, mix){
+                console.log(mix);
+                if(err || mix === null) res.sendStatus(403);
+                else{
+                    mix.remove();
+                    res.sendStatus(200);
+                }
+            });
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.put("/mix", function(req, res){
+    if (!req.body) return res.sendStatus(400)
+    var json = req.body;
+    var query  = User.where({ pseudo: json.owner });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        if(user.role !== undefined && user.role === "member" || user.role === "moderator" || user.role === "admin"){
+            var query  = Mix.where({ name: json.name, owner: user.pseudo });
+            query.findOne(function(err, mix){
+                if(err || mix === null) res.sendStatus(403);
+                else{
+                    mix.data = json.data;
+                    mix.save(function (err) {
+                        res.sendStatus(201);
+                    });
+                }
+            });
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.get("/star", function(req, res){
+    Star.find(function(error, results){
+        if(error) throw error;
+        else{
+            res.statusCode = 200;
+            res.send(results);
+        }
+    })
+});
+
+app.get("/star/:mixName", function(req, res){
+    var query  = Star.where({ mixName: req.params.mixName });
+    query.find(function (err, stars) {
+        if (err) throw err;
+        else{
+            res.statusCode = 200;
+            res.send(stars);
+        }
+    });
+});
+
+app.get("/star/:mixName/:userName", function(req, res){
+    var query  = Star.where({ mixName: req.params.mixName, userName: req.params.userName });
+    query.find(function (err, stars) {
+        if (err) throw err;
+        else{
+            res.statusCode = 200;
+            res.send(stars);
+        }
+    });
+});
+
+app.post("/star", function(req, res){
+    if (!req.body) return res.sendStatus(400)
+    var json = req.body;
+    var query  = User.where({ pseudo: json.userName });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        if (user.role !== undefined && user.role === "member" || user.role === "moderator" || user.role === "admin"){
+            query = Star.where({ mixName: json.mixName, userName: json.userName });
+            query.findOne(function(err, star){
+                if(star === null){
+                    var starsave = new Star({mixName: json.mixName, userName: json.userName, star: json.star}) ;
+                    starsave.save(function (err) {
+                        if (err) throw err;
+                        else
+                            res.sendStatus(201);
+                    });
+                }
+                else{
+                    res.sendStatus(403);
+                }
+            });
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.put("/star", function(req, res){
+    if (!req.body) return res.sendStatus(400)
+    var json = req.body;
+    var query  = User.where({ pseudo: json.userName });
+    query.findOne(function (err, user) {
+        if (err) throw err;
+        if(user.role !== undefined && user.role === "member" || user.role === "moderator" || user.role === "admin"){
+            var query  = Star.where({ mixName: json.mixName, userName: json.userName });
+            query.findOne(function(err, star){
+                star.star = json.star;
+
+                star.save(function (err) {
+                    res.sendStatus(201);
+                });
+            });
+        }
+        else{
+            res.sendStatus(403);
+        }
+    });
+});
+
+app.listen(8080);
---
+++ server/servertest.js
@@ -0,0 +1,537 @@
+/*jslint node: true */
+/*jslint mocha: true */
+'use strict';
+
+var assert = require('assert');
+var mongoose = require('mongoose');
+var request = require("supertest");
+
+mongoose.connect("mongodb://xythe.xyz:27017/mixmaze");
+
+
+var userSchema = {
+    pseudo: String,
+    password: String,
+    email: String,
+    role: String
+};
+
+var mixSchema = {
+    owner: String,
+    name: String,
+    music: String,
+    data : String
+};
+
+var musicSchema = {
+    musicName: String,
+    musicPath: String,
+    musicFiles : [String]
+};
+
+var starSchema = {
+    mixName: String,
+    userName: String,
+    star: Number
+}
+
+var User = mongoose.model('User', userSchema, 'user');
+var Music = mongoose.model('Music', musicSchema, 'music');
+var Mix = mongoose.model('Mix', mixSchema, 'mix');
+var Star = mongoose.model('Star', starSchema, 'star');
+
+var url = "";
+
+var user = {};
+var mix = {};
+var star = {};
+
+var userAPI = "";
+var mixAPI = "";
+var musicAPI = "";
+var starAPI = "";
+
+describe('Serveur-side Tests', function() {
+	before(function(done) {
+		url = "http://localhost:8080";
+		user.pseudo = "lovemocha";
+		user.email = "test@mocha.test";
+		user.password = "lovemocha";
+		user.role = "member";
+
+		mix.name = "lovemocha";
+		mix.music = "marco";
+		mix.data = "";
+		mix.owner = "admin";
+
+		star.mixName = "testmocha";
+		star.userName = "admin";
+		star.star = 3;
+
+		userAPI = "/users";
+		mixAPI = "/mix";
+		musicAPI = "/musics";
+		starAPI = "/star";
+
+    	done();
+  	});
+
+	describe("Simple test of GET apis", function(){
+
+	  	describe("GET /users", function(){
+	  		it('should respond with an array of users', function(done){
+	  			request(url)
+	  			.get(userAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body instanceof Array);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /mix", function(){
+	  		it('should respond with an array of mix', function(done){
+	  			request(url)
+	  			.get(mixAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body instanceof Array);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /musics", function(){
+	  		it('should respond with an array of musics', function(done){
+	  			request(url)
+	  			.get(musicAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body instanceof Array);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /star", function(){
+	  		it('should respond with an array of stars', function(done){
+	  			request(url)
+	  			.get(starAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body instanceof Array);
+	  				done();
+	  			});
+	  		});
+	  	});
+	});
+	//********************  ADVANCED TESTS FOR USERS *************************//
+	describe("Users tests", function(){
+		var numberOfUsers = 0;
+
+	  	describe("GET /users to fetch the number of users", function(){
+	  		it('should retrieve user number in database', function(done){
+	  			request(url)
+	  			.get(userAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				numberOfUsers = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("POST / users to create a new user", function(){
+	  		it('/should create a new user an check if the number of users has changed', function(done){
+	  			request(url)
+	  			.post(userAPI)
+	  			.send(user)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 201);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /users to fetch the number of users", function(){
+	  		it('should be the old number plus one', function(done){
+	  			request(url)
+	  			.get(userAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body.length === numberOfUsers+1);
+	  				numberOfUsers = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+
+
+	  	describe("POST / users to create the same user", function(){
+	  		it('/should return status code 403 and not create the user', function(done){
+	  			request(url)
+	  			.post(userAPI)
+	  			.send(user)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /users to fetch the number of users", function(){
+	  		it('should be the same number of users because creation failed', function(done){
+	  			request(url)
+	  			.get(userAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body.length === numberOfUsers);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /users/pseudo to fetch the user created before", function(){
+	  		it('should return the user and status code 200', function(done){
+	  			request(url)
+	  			.get(userAPI+"/"+user.pseudo)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body.length === 1);
+	  				assert(true, user.pseudo === res.body.pseudo);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("PUT /users to update a user role", function(){
+	  		it('should return status code 403 because only admin can update users', function(done){
+	  			request(url)
+	  			.put(userAPI)
+	  			.send({applicant:"member", pseudo: user.pseudo, role: "moderator"})
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("PUT /users to update a user role", function(){
+	  		it('should return status code 201 because the admin can update users', function(done){
+	  			request(url)
+	  			.put(userAPI)
+	  			.send({applicant:"admin", pseudo: user.pseudo, role: "moderator"})
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 201);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("DELETE /users to delete a user", function(){
+	  		it('should return status code 403 because only admin can delete users', function(done){
+	  			request(url)
+	  			.delete(userAPI+"/"+user.pseudo+"/toto")
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+
+	  	describe("DELETE /users to delete a user", function(){
+	  		it('should return status code 200 because the admin deleted this user', function(done){
+	  			request(url)
+	  			.delete(userAPI+"/"+"admin"+"/"+user.pseudo)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				done();
+	  			});
+	  		});
+	  	});
+	});
+
+	describe("Test to fetch a specfic music", function(){
+		describe("GET /musics/musicName", function(){
+	  		it('should return a single music', function(done){
+	  			request(url)
+	  			.get(musicAPI+"/marco")
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body.length === 1);
+	  				done();
+	  			});
+	  		});
+	  	});
+	});
+
+	//********************  ADVANCED TESTS FOR MIX *************************//
+	describe("Mix tests", function(){
+
+		var numberOfMix = 0;
+		var numberOfSpecificMusic = 0;
+		describe("GET /mix to fetch the number of mix", function(){
+	  		it('should retrieve mix number in database', function(done){
+	  			request(url)
+	  			.get(mixAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				numberOfMix = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /mix related to a music", function(){
+	  		it('should be return status code 200 and a array of mix', function(done){
+	  			request(url)
+	  			.get(mixAPI+"/"+mix.music)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				numberOfSpecificMusic = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("POST / mix to create a new mix", function(){
+	  		it('/should create a new mix an check if the number of mix has changed', function(done){
+	  			request(url)
+	  			.post(mixAPI)
+	  			.send(mix)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 201);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /mix related to a music after creation", function(){
+	  		it('should be return status code 200 and a array of mix plus the new mix', function(done){
+	  			request(url)
+	  			.get(mixAPI+"/"+mix.music)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, numberOfSpecificMusic+1 === res.body.length)
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /mix to fetch the number of mix", function(){
+	  		it('should be the old number plus one', function(done){
+	  			request(url)
+	  			.get(mixAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body.length === numberOfMix+1);
+	  				numberOfMix = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+
+
+	  	describe("POST /mix to create the same mix", function(){
+	  		it('/should return status code 403 and not create the mix', function(done){
+	  			request(url)
+	  			.post(mixAPI)
+	  			.send(mix)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /mix to fetch the number of mix", function(){
+	  		it('should be the same number of mix because creation failed', function(done){
+	  			request(url)
+	  			.get(mixAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body.length === numberOfMix);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+
+	  	describe("PUT /mix to update a mix data", function(){
+	  		it('should return status code 403 because only owner can update mix', function(done){
+	  			request(url)
+	  			.put(mixAPI)
+	  			.send({owner:"toto", name: mix.name})
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("PUT /mix to update a mix data", function(){
+	  		it('should return status code 201 because the owner can update his mix', function(done){
+	  			request(url)
+	  			.put(mixAPI)
+	  			.send({owner: mix.owner, name: mix.name, data: "newData"})
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 201);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("DELETE /mix to delete a mix", function(){
+	  		it('should return status code 403 because only owner can delete his mix', function(done){
+	  			request(url)
+	  			.delete(mixAPI+"/"+mix.name+"/toto")
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+
+	  	describe("DELETE /mix to delete a mix", function(){
+	  		it('should return status code 200 because the owner deleted his mix', function(done){
+	  			request(url)
+	  			.delete(mixAPI+"/"+mix.name+"/"+mix.owner)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				done();
+	  			});
+	  		});
+	  	});
+	});
+
+//********************  ADVANCED TESTS FOR STARS *************************//
+	describe("Stars tests", function(){
+
+		var totalStars = 0;
+		var totalStarsForAmix = 0;
+		var totalStarsForAmixByUser = 0;
+		var starScoreUpdate = 5;
+
+		describe("GET /star to fetch the number of star", function(){
+	  		it('should retrieve star number in database', function(done){
+	  			request(url)
+	  			.get(starAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				totalStars = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+	  	describe("GET /star/mixname to fetch the number of star for a mix", function(){
+	  		it('should retrieve star number in database for a mix', function(done){
+	  			request(url)
+	  			.get(starAPI+"/"+star.mixName)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				totalStarsForAmix = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+	  	describe("GET /star/mixName/userName to fetch the number of star for a mix for a user", function(){
+	  		it('should retrieve star number in database', function(done){
+	  			request(url)
+	  			.get(starAPI+"/"+star.mixName+"/"+star.userName)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				totalStarsForAmixByUser = res.body.length;
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("POST /star to create a star", function(){
+	  		it('/should create a new star raw an check if the number of mix has changed', function(done){
+	  			request(url)
+	  			.post(starAPI)
+	  			.send(star)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 201);
+	  				done();
+	  			});
+	  		});
+	  	});
+
+	  	describe("GET /star to fetch the number of star", function(){
+	  		it('should increase by one', function(done){
+	  			request(url)
+	  			.get(starAPI)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, totalStars+1 === res.body.length);
+	  				done();
+	  			});
+	  		});
+	  	});
+	  	describe("GET /star/mixname to fetch the number of star for a mix", function(){
+	  		it('should increase by one', function(done){
+	  			request(url)
+	  			.get(starAPI+"/"+star.mixName)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, totalStarsForAmix+1 === res.body.length);
+	  				done();
+	  			});
+	  		});
+	  	});
+	  	describe("GET /star/mixName/userName to fetch the number of star for a mix for a user", function(){
+	  		it('should increase by one', function(done){
+	  			request(url)
+	  			.get(starAPI+"/"+star.mixName+"/"+star.userName)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, totalStarsForAmixByUser+1 === res.body.length);
+	  				done();
+	  			});
+	  		});
+	  	});
+	  	describe("POST /star to create the same star", function(){
+	  		it('/should return status code 403 because already exists', function(done){
+	  			request(url)
+	  			.post(starAPI)
+	  			.send(star)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+	  	describe("PUT /star to update a star score", function(){
+	  		it('/should return status code 201', function(done){
+	  			star.star = starScoreUpdate;
+	  			request(url)
+	  			.post(starAPI)
+	  			.send(star)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 403);
+	  				done();
+	  			});
+	  		});
+	  	});
+	  	describe("GET /star/mixName/userName to check the update", function(){
+	  		it('/should return status code 200 and star with star = 5', function(done){
+	  			request(url)
+	  			.get(starAPI+"/"+star.mixName+"/"+star.userName)
+	  			.end(function(err, res){
+	  				assert.equal(res.statusCode, 200);
+	  				assert(true, res.body.star === starScoreUpdate);
+	  				done();
+	  			});
+	  		});
+	  	});
+	});
+
+	after(function(done){
+		Star.remove({mixName: "testmocha"}, function(err){console.log(err);});
+		done();
+  	});
+});
